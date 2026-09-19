e29796b823b5792635da928bc2e3d2d3db967483
Allgaeuer-Bua <151471966+Allgaeuer-Bua@users.noreply.github.com>
2026-09-19 06:48:44 +0200

    Explain controls and calibration status

--- custom_components/speicher_ladelogik/const.py
+++ custom_components/speicher_ladelogik/const.py
@@ -8,7 +8,7 @@ from homeassistant.const import Platform
 
 DOMAIN: Final = "speicher_ladelogik"
 NAME: Final = "Speicher-Ladelogik"
-VERSION: Final = "1.0.1"
+VERSION: Final = "1.0.2"
 
 PLATFORMS: Final = [
     Platform.SENSOR,
